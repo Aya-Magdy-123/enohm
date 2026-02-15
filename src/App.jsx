9349446744204db1402ac8a13d631bbd9bc35a26
ayamagdy <am3839593@gmail.com>
2026-02-15 05:30:08 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import EnohmForm from './enohmForm'
+import EnohmForm from './EnohmForm'
+
 function App() {
   const [count, setCount] = useState(0)
 
